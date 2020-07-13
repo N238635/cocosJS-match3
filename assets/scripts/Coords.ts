e57151767a29eb7b40f5fe515dbece8655e9fbad
Nickers_ <lnickersl@gmail.com>
2020-07-13 14:01:07 +0500

    addition and subtraction methods

--- assets/scripts/Coords.ts
+++ assets/scripts/Coords.ts
@@ -22,4 +22,36 @@ export default class Coords {
     public get y(): number {
         return this._y;
     }
+
+    public addSelf(coords: Coords | number, y?: number): Coords {
+        if (typeof(coords) !== 'number') {
+            this._x += coords.x;
+            this._y += coords.y;
+        } else {
+            this._x += coords;
+            this._y += y;
+        }
+        return this;
+    }
+
+    public add(coords: Coords | number, y?: number): Coords {
+        if (typeof(coords) !== 'number') return new Coords(this.x + coords.x, this.y + coords.y);
+        else return new Coords(this.x + coords, this.y + y);
+    }
+
+    public subSelf(coords: Coords | number, y?: number): Coords {
+        if (typeof(coords) !== 'number') {
+            this._x -= coords.x;
+            this._y -= coords.y;
+        } else {
+            this._x -= coords;
+            this._y -= y;
+        }
+        return this;
+    }
+
+    public sub(coords: Coords | number, y?: number): Coords {
+        if (typeof(coords) !== 'number') return new Coords(this.x - coords.x, this.y - coords.y);
+        else return new Coords(this.x - coords, this.y - y);
+    }
 }
