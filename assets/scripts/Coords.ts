eaf44cd5f429137da783a73933567268fe76ff71
Nickers_ <lnickersl@gmail.com>
2020-07-13 14:28:21 +0500

    refactored class methods

--- assets/scripts/Coords.ts
+++ assets/scripts/Coords.ts
@@ -1,57 +1,43 @@
 export default class Coords {
-    private _x: number = 0;
-    private _y: number = 0;
+    public col: number = 0;
+    public row: number = 0;
 
-    constructor(x: number = 0, y: number = 0) {
-        this._x = x;
-        this._y = y;
+    constructor(col: number = 0, row: number = 0) {
+        this.col = col;
+        this.row = row;
     }
 
-    public set x(value: number) {
-        this._x = value;
-    }
-
-    public get x(): number {
-        return this._x;
-    }
-
-    public set y(value: number) {
-        this._y = value;
-    }
-
-    public get y(): number {
-        return this._y;
-    }
-
-    public addSelf(coords: Coords | number, y?: number): Coords {
-        if (typeof(coords) !== 'number') {
-            this._x += coords.x;
-            this._y += coords.y;
-        } else {
-            this._x += coords;
-            this._y += y;
-        }
+    public addSelf(coords: Coords): Coords {
+        this.col += coords.col;
+        this.row += coords.row;
         return this;
     }
 
-    public add(coords: Coords | number, y?: number): Coords {
-        if (typeof(coords) !== 'number') return new Coords(this.x + coords.x, this.y + coords.y);
-        else return new Coords(this.x + coords, this.y + y);
+    public add(coords: Coords): Coords {
+        return this.clone().addSelf(coords);
     }
 
-    public subSelf(coords: Coords | number, y?: number): Coords {
-        if (typeof(coords) !== 'number') {
-            this._x -= coords.x;
-            this._y -= coords.y;
-        } else {
-            this._x -= coords;
-            this._y -= y;
-        }
+    public subSelf(coords: Coords): Coords {
+        this.col -= coords.col;
+        this.row -= coords.row;
         return this;
     }
 
-    public sub(coords: Coords | number, y?: number): Coords {
-        if (typeof(coords) !== 'number') return new Coords(this.x - coords.x, this.y - coords.y);
-        else return new Coords(this.x - coords, this.y - y);
+    public sub(coords: Coords): Coords {
+        return this.clone().subSelf(coords);
+    }
+
+    public mulSelf(coords: Coords): Coords {
+        this.col *= coords.col;
+        this.row *= coords.row;
+        return this;
+    }
+
+    public mul(coords: Coords): Coords {
+        return this.clone().mulSelf(coords);
+    }
+
+    public clone(): Coords {
+        return new Coords(this.col, this.row);
     }
 }
