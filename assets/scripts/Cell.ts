e9df997a0c10619d4eb0243f7dbfef2b57d8d450
Nickers_ <lnickersl@gmail.com>
2020-07-14 13:50:03 +0500

    typeID replaced with isDark fixed sprite change

--- assets/scripts/Cell.ts
+++ assets/scripts/Cell.ts
@@ -1,34 +1,26 @@
 import Coords from "./Coords";
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Cell extends cc.Component {
 
     @property(cc.SpriteFrame) whiteSpriteFrame: cc.SpriteFrame = null;
     @property(cc.SpriteFrame) greySpriteFrame: cc.SpriteFrame = null;
+    @property(cc.Sprite) background: cc.Sprite = null;
 
-    private _sprite: cc.Sprite;
-    private _typeID: number;
+    public isDisabled: boolean = true;
+
+    set isDark(isDark: boolean) {
+        this._isDark = isDark;
+        this.updateSpriteFrame();
+    }
+
+    get isDark(): boolean { return this._isDark }
 
     private _coords: Coords = new Coords();
+    private _isDark: boolean;
 
-    private changeSpriteFrame(typeID: number): void {
-        switch (typeID) {
-            case 0:
-                this._sprite.spriteFrame = this.whiteSpriteFrame;
-                break;
-            case 1:
-                this._sprite.spriteFrame = this.greySpriteFrame;
-                break;
-        }
-    }
-
-    private getPositionOfCoords(coords: Coords): cc.Vec2 {
-        //TODO
-        return cc.v2(62 * (coords.col - 4), 62 * (4 - coords.row));
-    }
-
-    public changePosition(): void {
+    public updatePosition(): void {
         let pos = this.getPositionOfCoords(this._coords);
         this.node.setPosition(pos);
     }
@@ -37,21 +29,19 @@ export default class Cell extends cc.Component {
         return this._coords;
     }
 
-    public setCoords(col: Coords | number, row?: number ) {
-        if (typeof(col) !== 'number') this._coords = col;
-        else this._coords = new Coords(col, row);
+    public setCoords(coords: Coords) {
+        this._coords = coords;
+        this.updatePosition();
     }
 
-    public setType(typeID: number): void {
-        this._typeID = typeID;
-        this.changeSpriteFrame(this._typeID);
+    private updateSpriteFrame(): void {
+        if (!this.isDisabled) {
+            this.background.spriteFrame = this.isDark ? this.greySpriteFrame : this.whiteSpriteFrame;
+        }
     }
 
-    get typeID(): number {
-        return this._typeID;
-    }
-
-    protected onLoad(): void {
-        this._sprite = this.node.addComponent(cc.Sprite);
+    private getPositionOfCoords(coords: Coords): cc.Vec2 {
+        //TODO
+        return cc.v2(62 * (coords.col - 4), 62 * (4 - coords.row));
     }
 }
