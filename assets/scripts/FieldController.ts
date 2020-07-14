569743a841dac0d5e432885e0f3e8da6ef9eecdf
Nickers_ <lnickersl@gmail.com>
2020-07-14 12:20:56 +0500

    added getCell method get fieldLayout from config

--- assets/scripts/FieldController.ts
+++ assets/scripts/FieldController.ts
@@ -7,34 +7,30 @@ const { ccclass, property } = cc._decorator;
 export default class FieldController extends cc.Component {
 
     @property(cc.Prefab) cellPrefab: cc.Prefab = null;
+    @property(cc.JsonAsset) config: cc.JsonAsset = null;
 
-    private readonly _fieldLayout: number[][] = [
-        [1, 1, 1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
+    private _fieldLayout: number[][] = [];
+    private _rows: number = 0;
+    private _columns: number = 0;
 
-    private readonly _height: number = 9;
-    private readonly _width: number = 9;
+    private _field: Cell[][] = [];
 
-    private _field: cc.Node[][] = [];
-
-    private everyCoord(callback: (col: number, row: number) => void): void {
-        for (let row = 0; row < this._height; row++) {
-            for (let col = 0; col < this._width; col++) {
-                callback(col, row);
+    private everyCell(callback: (cell: Cell) => void): void {
+        for (let row = 0; row < this._rows; row++) {
+            for (let col = 0; col < this._columns; col++) {
+                callback(this.getCell(col, row));
             }
         }
     }
 
-    public getCell(coords: Coords): Cell {
-        return
+    public getCell(coords: Coords | number, row?: number): Cell {
+        const isInvalidType: boolean = (typeof(coords) === 'number');
+        const column = isInvalidType && (coords as number);
+
+        let realCoords = isInvalidType ? new Coords(column, row) : (coords as Coords);
+        if (!realCoords) return;
+
+        return this._field[realCoords.col][realCoords.row];
     }
 
     private isEven(n: number): boolean {
@@ -43,9 +39,9 @@ export default class FieldController extends cc.Component {
 
     // Заполняем поле клетками
     private initField(): void {
-        for (let row = 0; row < this._height; row++) {
+        for (let row = 0; row < this._rows; row++) {
             this._field.push([]);
-            for (let col = 0; col < this._width; col++) {
+            for (let col = 0; col < this._columns; col++) {
                 if (this._fieldLayout[row][col] === 1) {
                     let typeID = this.isEven(row) !== this.isEven(col) ? 0 : 1;
                     this._field[row].push(
@@ -58,8 +54,8 @@ export default class FieldController extends cc.Component {
 
     private printField(): void {
         let str = "";
-        for (let row = 0; row < this._height; row++) {
-            for (let col = 0; col < this._width; col++) {
+        for (let row = 0; row < this._rows; row++) {
+            for (let col = 0; col < this._columns; col++) {
                 str += this._fieldLayout[row][col] === 1 ? 'O ' : 'X ';
             }
             str += '\n';
@@ -71,15 +67,19 @@ export default class FieldController extends cc.Component {
         return cc.v2(62 * (coords.col - 4), 62 * (4 - coords.row));
     }
 
-    public createCell(coords: Coords, typeID: number): cc.Node {
+    public createCell(coords: Coords, typeID: number): Cell {
         let cell = cc.instantiate(this.cellPrefab);
         cell.parent = this.node;
         let pos = this.getPositionOfCell(coords);
         cell.setPosition(pos);
-        return cell;
+        return cell.getComponent(Cell);
     }
 
     protected onLoad(): void {
+        this._fieldLayout = this.config.json.fieldLayout;
+        this._rows = this.config.json.rows;
+        this._columns = this.config.json.columns;
+
         this.printField();
         this.initField();
     }
