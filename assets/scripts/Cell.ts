1072555556115d40cae1623b397d5ffbdec3c4e2
Nickers_ <lnickersl@gmail.com>
2020-07-14 12:57:18 +0500

    setType, changeSpriteFrame, changePosition methods

--- assets/scripts/Cell.ts
+++ assets/scripts/Cell.ts
@@ -7,15 +7,52 @@ export default class Cell extends cc.Component {
     @property(cc.SpriteFrame) whiteSpriteFrame: cc.SpriteFrame = null;
     @property(cc.SpriteFrame) greySpriteFrame: cc.SpriteFrame = null;
 
+    private _sprite: cc.Sprite;
+    private _typeID: number;
+
     private _coords: Coords = new Coords();
+
+    private changeSpriteFrame(typeID: number): void {
+        switch (typeID) {
+            case 0:
+                this._sprite.spriteFrame = this.whiteSpriteFrame;
+                break;
+            case 1:
+                this._sprite.spriteFrame = this.greySpriteFrame;
+                break;
+        }
+    }
+
+    private getPositionOfCoords(coords: Coords): cc.Vec2 {
+        //TODO
+        return cc.v2(0, 0);
+    }
+
+    private changePosition(): void {
+        let pos = this.getPositionOfCoords(this._coords);
+        this.node.setPosition(pos);
+    }
 
     public getCoords(): Coords {
         return this._coords;
     }
 
-    public setCoords(x: Coords | number, y?: number ) {
-        if (typeof(x) !== 'number') this._coords = x;
-        else this._coords = new Coords(x, y);
+    public setCoords(col: Coords | number, row?: number ) {
+        if (typeof(col) !== 'number') this._coords = col;
+        else this._coords = new Coords(col, row);
     }
 
+    public setType(typeID: number): void {
+        this._typeID = typeID;
+        this.changeSpriteFrame(this._typeID);
+    }
+
+    get typeID(): number {
+        return this._typeID;
+    }
+
+    protected onLoad(): void {
+        this._sprite = this.node.addComponent(cc.Sprite);
+        this.changePosition();
+    }
 }
