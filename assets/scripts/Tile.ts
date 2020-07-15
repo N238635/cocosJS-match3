edfcd84ba52bb98c8004deb2398579e5ccf46db4
Nickers_ <lnickersl@gmail.com>
2020-07-15 11:51:10 +0500

    setColor, setSprite, setColorID, setType tileType, colors

--- assets/scripts/Tile.ts
+++ assets/scripts/Tile.ts
@@ -1,28 +1,67 @@
 import Coords from "./Coords";
 const {ccclass, property} = cc._decorator;
 
-export enum tileColor {
-    Yellow = 0,
-    Red = 1, 
-    Blue = 2, 
-    Purple = 3, 
-    Green = 4, 
-    Orange = 5
+export enum tileColorID {
+    White = 0,
+    Yellow = 1,
+    Red = 2, 
+    Blue = 3, 
+    Purple = 4, 
+    Green = 5, 
+    Orange = 6
 }
+
+export enum tileType {
+    Gem = 0,
+    Vertical = 1,
+    Horizontal = 2,
+    Rainbow = 3
+}
+
+const colors = {
+    0: [255, 255, 255],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: []
+};
 
 @ccclass
 export default class Tile extends cc.Component {
     
+    @property(cc.Sprite) sprite: cc.Sprite = null;
     @property(cc.SpriteFrame) whiteCircle: cc.SpriteFrame = null;
 
-    private _coords: Coords = new Coords();
+    get coords(): Coords { return this._coords }
+    set coords(coords: Coords) { this._coords = coords }
 
-    public getCoords(): Coords {
-        return this._coords;
+    private _coords: Coords = new Coords();
+    private _colorID: tileColorID = tileColorID.White;
+    private _type: tileType = tileType.Gem;
+
+    public setType(type: tileType): void {
+        this._type = type;
+        this.setSprite();
     }
 
-    public setCoords(x: Coords | number, y?: number ) {
-        if (typeof(x) !== 'number') this._coords = x;
-        else this._coords = new Coords(x, y);
+    public setColorID(colorID: tileColorID): void {
+        this._colorID = colorID;
+        this.setColor();
+    }
+
+    private setSprite(): void {
+        switch (this._type) {
+            case 0:
+                this.sprite.spriteFrame = this.whiteCircle;
+                break;
+        }
+    }
+
+    private setColor(): void {
+        if (colors[this._colorID]) {
+            this.node.color = cc.color(...colors[this._colorID]);
+        }
     }
 }
