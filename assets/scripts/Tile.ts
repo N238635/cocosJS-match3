9ca91504cc5f66c5ac99702280596b5fb234863e
Nickers_ <lnickersl@gmail.com>
2020-07-15 11:11:35 +0500

    colors enum sprite frame

--- assets/scripts/Tile.ts
+++ assets/scripts/Tile.ts
@@ -1,9 +1,20 @@
 import Coords from "./Coords";
 const {ccclass, property} = cc._decorator;
 
+export enum tileColor {
+    Yellow = 0,
+    Red = 1, 
+    Blue = 2, 
+    Purple = 3, 
+    Green = 4, 
+    Orange = 5
+}
+
 @ccclass
 export default class Tile extends cc.Component {
     
+    @property(cc.SpriteFrame) whiteCircle: cc.SpriteFrame = null;
+
     private _coords: Coords = new Coords();
 
     public getCoords(): Coords {
