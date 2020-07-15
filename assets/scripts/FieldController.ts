10ccb0e4ac0c0ef3cbed0a62211f943574e29d6e
Nickers_ <lnickersl@gmail.com>
2020-07-15 12:28:59 +0500

    tilePrefab, createTile config variables reworked

--- assets/scripts/FieldController.ts
+++ assets/scripts/FieldController.ts
@@ -1,28 +1,23 @@
 import Coords from "./Coords";
 import Cell from "./Cell";
-import GameController from "./GameController";
+import Tile, { tileColorID, tileType } from "./Tile";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class FieldController extends cc.Component {
 
-    @property(cc.Prefab) cellPrefab: cc.Prefab = null;
     @property(cc.JsonAsset) config: cc.JsonAsset = null;
 
-    private _fieldLayout: number[][] = [];
-    private _rows: number = 0;
-    private _columns: number = 0;
-    private _cellSize: number = 0;
-
-    private _canvasWidth: number;
-    private _canvasHeight: number;
+    @property(cc.Prefab) cellPrefab: cc.Prefab = null;
+    @property(cc.Prefab) tilePrefab: cc.Prefab = null;
 
     private _field: Cell[][] = [];
 
     public everyCell(callback: (cell: Cell) => void): void {
-        for (let row = 0; row < this._rows; row++) {
-            for (let col = 0; col < this._columns; col++) {
+        const {columns, rows} = this.config.json;
+        for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < columns; col++) {
                 callback(this.getCell(col, row));
             }
         }
@@ -38,43 +33,35 @@ export default class FieldController extends cc.Component {
         return this._field[realCoords.col][realCoords.row];
     }
 
-    public createCell(coords: Coords, isDisabled: boolean, isDark: boolean): Cell {
-        let cell = cc.instantiate(this.cellPrefab).getComponent(Cell);
-        cell.node.parent = this.node;
-        cell.setSize(this._cellSize);
-        cell.node.setPosition(this.getPositionOfCoords(coords));
-        cell.coords = coords;
-        cell.isDisabled = isDisabled;
-        cell.isDark = isDark;
-        return cell;
+    public createTile(type: tileType, colorID: tileColorID = tileColorID.White): Tile {
+        let tile = cc.instantiate(this.tilePrefab).getComponent(Tile);
+        tile.node.parent = this.node;
+        tile.setType(type);
+        tile.setColorID(colorID);
+        return;
     }
 
     protected onLoad(): void {
-        this._fieldLayout = this.config.json.fieldLayout;
-        this._rows = this.config.json.rows;
-        this._columns = this.config.json.columns;
-        this._cellSize = this.config.json.cellSize;
-        this._canvasWidth = this.config.json.canvas.width;
-        this._canvasHeight = this.config.json.canvas.height;
-
         this.printField();
         this.initField();
     }
 
     private getPositionOfCoords(coords: Coords): cc.Vec2 {
-        let absoluteX = this._canvasWidth / 2 - (this._columns / 2 - (coords.col + 0.5)) * this._cellSize;
-        let absoluteY = this._canvasHeight / 2 + (this._rows / 2 - (coords.row + 0.5)) * this._cellSize;
+        const {columns, rows, canvas, cellSize} = this.config.json;
+        let absoluteX = canvas.width / 2 - (columns / 2 - (coords.col + 0.5)) * cellSize;
+        let absoluteY = canvas.height / 2 + (rows / 2 - (coords.row + 0.5)) * cellSize;
         return this.node.convertToNodeSpaceAR(cc.v2(absoluteX, absoluteY));
     }
 
     // Заполняем поле клетками
     private initField(): void {
+        const {columns, rows, fieldLayout} = this.config.json;
         let isDark: boolean;
         let isDisabled: boolean;
-        for (let row = 0; row < this._rows; row++) {
+        for (let row = 0; row < rows; row++) {
             this._field.push([]);
-            for (let col = 0; col < this._columns; col++) {
-                isDisabled = (this._fieldLayout[row][col] === 0);
+            for (let col = 0; col < columns; col++) {
+                isDisabled = (fieldLayout[row][col] === 0);
                 isDark = (this.isEven(row) === this.isEven(col));
                 this._field[row].push(
                     this.createCell(new Coords(col, row), isDisabled, isDark)
@@ -83,11 +70,24 @@ export default class FieldController extends cc.Component {
         }
     }
 
+    private createCell(coords: Coords, isDisabled: boolean, isDark: boolean): Cell {
+        const { cellSize } = this.config.json;
+        let cell = cc.instantiate(this.cellPrefab).getComponent(Cell);
+        cell.node.parent = this.node;
+        cell.setSize(cellSize);
+        cell.node.setPosition(this.getPositionOfCoords(coords));
+        cell.coords = coords;
+        cell.isDisabled = isDisabled;
+        cell.isDark = isDark;
+        return cell;
+    }
+
     private printField(): void {
+        const { fieldLayout, rows, columns} = this.config.json;
         let str = "";
-        for (let row = 0; row < this._rows; row++) {
-            for (let col = 0; col < this._columns; col++) {
-                str += this._fieldLayout[row][col] === 1 ? 'O ' : 'X ';
+        for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < columns; col++) {
+                str += fieldLayout[row][col] === 1 ? 'O ' : 'X ';
             }
             str += '\n';
         }
