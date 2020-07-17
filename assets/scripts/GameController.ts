2ef5a445907c1c911b089cbf712985b23490318c
Nickers_ <lnickersl@gmail.com>
2020-07-17 10:18:23 +0500

    mouseEvent test

--- assets/scripts/GameController.ts
+++ assets/scripts/GameController.ts
@@ -11,6 +11,14 @@ export default class GameController extends cc.Component {
     @property(FieldController) field: FieldController = null;
 
     protected onLoad(): void {
+        this.node.on(cc.Node.EventType.MOUSE_DOWN, (event: cc.Event.EventMouse) => {
+            let absolutePosition: cc.Vec2 = event.getLocation();
+            cc.log(absolutePosition.x, absolutePosition.y);
+            let fieldCoords: Coords = this.field.getCoordsFromAbsolutePosition(absolutePosition);
+            cc.log(fieldCoords.col, fieldCoords.row);
+
+        });
+
         this.field.printField();
         this.field.initField();
 
